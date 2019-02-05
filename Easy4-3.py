cc51f29fa3208de6b046d8037fe9cc989fd9024f
Entitie <entitie@yandex.ru>
2019-02-05 16:37:25 +0300

    ...

--- Easy4-3.py
+++ Easy4-3.py
@@ -6,10 +6,18 @@
 # + Элемент не кратен 4
 import random
 n = int(input('Введите количество произвольных чисел: '))
-m = int(input('Введите верхний предел: '))
-list_ = [random.randint(0, m) for _ in range(n)]
+m = int(input('Введите предел диапазона(по модулю): '))
+list_ = [random.randint(-m, m) for _ in range(n)]
 print('Ваш список произвольных чисел =', list_)
 
-for _ in
-new_list_ = [ list_ ** 2 for qn in list_]
+new_list_ = []
+for el in list_:
+    # new_list_.append(list_[el] if list_[el] % 3 == 0 and list_[el] > 0 and list_[el] % 4 != 0)
+    print(list_[el])
+    if ((list_[el] % 3) == 0) and (list_[el] > 0) and ((list_[el] % 4) != 0):
+        new_list_.append(list_[el])
+
+#new_list_ = [list_[el] if list_[el] % 3 == 0 and (list_[el] > 0) and (list_[_] % 4 != 0) for el in list_]
+
+#  and (list_[_] % 2 == 0) and (list_[_] // 4 > 0)
 print('Список из элементов исходного, удовлетворяющих условиям =', new_list_)
